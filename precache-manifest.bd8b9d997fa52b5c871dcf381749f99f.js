8d472e1554b8fc3efcd591d94b12c13825e06f39
pylnata <pnataliya@icloud.com>
2019-10-21 15:21:59 +0200

    Updates

---
+++ precache-manifest.bd8b9d997fa52b5c871dcf381749f99f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74f7cf65629d1f767709d1b5a8d4b81e",
+    "url": "/livebook/index.html"
+  },
+  {
+    "revision": "42b5fd72d38b584eae51",
+    "url": "/livebook/static/css/main.f87fa4be.chunk.css"
+  },
+  {
+    "revision": "6943b7f56945e7014db2",
+    "url": "/livebook/static/js/2.c2f9cdff.chunk.js"
+  },
+  {
+    "revision": "42b5fd72d38b584eae51",
+    "url": "/livebook/static/js/main.86a96bfe.chunk.js"
+  },
+  {
+    "revision": "fb0e047f1363193023d2",
+    "url": "/livebook/static/js/runtime-main.320e36ce.js"
+  }
+]);
